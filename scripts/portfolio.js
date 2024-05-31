92bd9a99c58e888bcd89d6ff85994a66661bf7c1
Jonbasha <jb56113@ubt-uni.net>
2024-05-31 16:35:14 +0200

    Added new movie + created main page

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const projectUrls = {
                 'Dark': 'darkMain.html',
                 'Dune': 'duneMain.html',
+                'Spectre': 'spectreMain.html'
                 // Add more project names and URLs as needed
             };
 
