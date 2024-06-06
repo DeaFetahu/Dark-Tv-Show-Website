3de9acb7f429cc74fa84f7f0fad7aa7da1b08500
DeaFetahu <deafetahu2@gmail.com>
2024-06-06 14:17:46 +0200

    added a new tv show + fixed the problem with pixi- Read the description !
    Install on VS Code Live server(it has a purple logo like the mobile data logo but purple) and then on live server you can see the project fully

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,14 +1,23 @@
 let currentSlideIndex = 0;
 const slidesToShow = 5;
-const totalSlides = document.querySelectorAll('.slider-item').length;
+const sliderItems = document.querySelectorAll('.slider-item');
+const totalSlides = sliderItems.length;
 const sliderWrapper = document.getElementById('slider-wrapper');
+
+// Set the width of the slider wrapper to accommodate all slides
+sliderWrapper.style.width = `${(totalSlides / slidesToShow) * 100}%`;
+
+// Set the width of each slider item
+sliderItems.forEach(item => {
+    item.style.width = `${100 / totalSlides}%`;
+});
 
 function moveSlide(step) {
     currentSlideIndex += step * slidesToShow;
     if (currentSlideIndex < 0) {
         currentSlideIndex = 0;
-    } else if (currentSlideIndex >= totalSlides) {
-        currentSlideIndex = 0;
+    } else if (currentSlideIndex > totalSlides - slidesToShow) {
+        currentSlideIndex = totalSlides - slidesToShow;
     }
-    sliderWrapper.style.transform = `translateX(-${currentSlideIndex * (100 / slidesToShow)}%)`;
+    sliderWrapper.style.transform = `translateX(-${(currentSlideIndex / totalSlides) * 100}%)`;
 }
